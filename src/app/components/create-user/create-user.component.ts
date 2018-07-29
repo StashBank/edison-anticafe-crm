6dede897368ab80766967e05fc376abc0f5f7a5b
Unknown <opavlovskyi@gmail.com>
2018-07-29 17:50:59 +0300

    create user improvements

--- src/app/components/create-user/create-user.component.ts
+++ src/app/components/create-user/create-user.component.ts
@@ -1,8 +1,9 @@
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { RegistrationValidator } from './registration-validator';
-
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-create-new-user',
@@ -15,15 +16,18 @@ export class CreateUserComponent implements OnInit {
   public createUserForm: FormGroup;
   public mask = ['(', /[0-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-' , /\d/, /\d/];
 
-  constructor( private formBuilder: FormBuilder,
-               private userService: UserService,
-              ) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    public snackBar: MatSnackBar,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit() {
     this.createUserForm = this.formBuilder.group({
       login: ['', [
           Validators.required,
-          Validators.pattern(/^([A-z][A-Za-z]*\s+[A-Za-z]*)|([A-z][A-Za-z]*)$/),
+          Validators.pattern(/^([A-z][A-Za-z]*\s+[A-Za-z]*\d{0,})|([A-z][A-Za-z]*\d{0,})$/),
         ]
       ],
       password: ['', [
@@ -52,22 +56,25 @@ export class CreateUserComponent implements OnInit {
   public registerNewUser() {
 
     const data = this.createUserForm.value;
-
-    this.userService.create(data).subscribe(response => {
-      // console.log('okay');
-      if (response.success) {
-        alert('Користувача створенно');
-      } else {
+    this.spinner.show();
+    this.userService.create(data)
+      .finally(() => this.spinner.hide())
+      .subscribe(response => {
+        // console.log('okay');
+        if (response.success) {
+          this.snackBar.open('Користувача створенно', null, {duration: 800 });
+          this.createUserForm.reset();
+        } else {
+          this.errorOnRegisterNewUser();
+        }
+      }, error => {
+        console.log(error);
         this.errorOnRegisterNewUser();
-      }
-    }, error => {
-      console.log(error);
-      this.errorOnRegisterNewUser();
-    });
+      });
   }
 
   errorOnRegisterNewUser() {
-    alert('Помилка під час створення користувача');
+    this.snackBar.open('Помилка під час створення користувача', null, { duration: 800 });
   }
 
 }
